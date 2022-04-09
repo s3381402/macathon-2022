4b6aeea62c0d3836b3cd3c6a1d868210034840f2
Peter Wu <82991172+s3381402@users.noreply.github.com>
2022-04-09 16:12:24 +1000

    added html pages

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+# Map page
+@app.route("/map")
+def map():
+    return render_template("map.html")
+
+# Report Issue page
+@app.route("/report_issue")
+def report_issue():
+    return render_template("report_issue.html")
